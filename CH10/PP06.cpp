f3f61415a092dc2f6ed6c026d1f2cddff680a106
JLJVS <72031471+JLJVS@users.noreply.github.com>
2026-07-25 23:49:47 +0200

    Temporarily gave up on programming projects Ch10, return later

--- CH10/PP06.cpp
+++ CH10/PP06.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+class Polynomial
+{
 
+    public:
+    private:
+        vector<double> coeffs;
+        int order;
+
+
+
+
+}
 
 int main()
 {
