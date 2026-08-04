272c0d7bb5a5ea13c04d6ba0026d9fbc45bf5af6
JLJVS <72031471+JLJVS@users.noreply.github.com>
2026-08-04 04:23:20 +0200

    Solution for Programming Project 14 Ch 12

--- CH12/PP14.cpp
+++ CH12/PP14.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 #include <fstream>
-using namespace std;
+#include <string>
+#include <iomanip>
+using std::cout, std::endl, std::ifstream, std::string, std::setw;
 
 
 int main()
-{
+{   
+    string fileName{"PP14data.txt"};
+    ifstream inputFile(fileName);
+    int lineNum {1};
+    bool lineStarted {false}, lineNumPrinted {false};
+    char next;
 
+    if (!inputFile) {
+        std::cerr << "Error: could not open " << fileName << ".txt\n";
+        return 1;
+    }
+
+    while (inputFile.get(next))
+    {   
+        if (!lineNumPrinted){
+            cout << setw(3) << lineNum << ": ";
+            lineNumPrinted = true;
+            lineNum++;
+        }
+        if (!lineStarted && (next == ' ')){
+            continue;
+        } else {
+            lineStarted = true;
+        }
+        cout << next;
+        if (next == '\n'){
+            lineStarted = false;
+            lineNumPrinted = false;
+        }
+    }
 
     return 0;
 }
