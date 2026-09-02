5a3a5c163600e5b66dc8d20d2fa56c9efddc5598
JLJVS <72031471+JLJVS@users.noreply.github.com>
2026-09-02 23:56:38 +0200

    Solutions for Self Test 01-02 Ch15

---
+++ CH15/ST01.cpp
@@ -0,0 +1,9 @@
+/*
+
+If a function has one or more virtual function it's an abstract class. 
+
+A virtual class has no definition (yet) in the base class and must be defined in the derived classes
+
+e.g. virtual void CheckEligibility () const = 0;
+
+*/
---
+++ CH15/ST02.cpp
@@ -0,0 +1,6 @@
+/*
+
+If we don't make the function virtual when we call the original class function on the object we get the original function instead of the derived class function
+
+
+*/
