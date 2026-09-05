840700f74c530962e83120d0ab1b6c99bf99891b
JLJVS <72031471+JLJVS@users.noreply.github.com>
2026-09-05 23:35:04 +0200

    Progress for Programming Project 03 Ch15

--- CH15/PP03.cpp
+++ CH15/PP03.cpp
@@ -5,3 +5,76 @@
 #include <iomanip>
 using std::cout, std::endl, std::string;
 using std::setw;
+
+class Organism{
+    public:
+        Organism() = default;
+        Organism(int startX, int startY) : x{startX}, y{startY}{}
+        virtual void move() = 0;
+        virtual ~Organism() = default;
+        virtual char symbol() const = 0;
+    protected:
+        int x {0};
+        int y {0};
+
+};
+
+class Ant : public Organism{
+    public:
+        Ant(int startX, int startY) : Organism{startX, startY} {}
+        void move() override {};
+        char symbol() const override {return 'O';}
+};
+
+class Doodlebug: public Organism{
+    public:
+        Doodlebug(int startX, int startY) : Organism{startX, startY} {}
+        void move() override {};
+        char symbol() const override {return 'X';}
+};
+
+class World
+{   
+    static constexpr int SIZE = 20;
+    public:
+        explicit World(int nDoodleBugs=5, int nAnts = 100);
+        ~World();
+        
+    protected:
+        Organism* grid[SIZE][SIZE] {};
+        void populate(int nDoodleBugs, int nAnts);
+
+};
+
+World::World(int nDoodlebugs, int nAnts) { populate(nDoodlebugs, nAnts);}
+
+World::~World()
+{
+    for (int r{}; r < SIZE; ++r){
+        for (int c{}; c < SIZE; ++c){
+            delete grid[r][c];
+        }
+    }
+}
+
+void World::populate(int nDoodleBugs, int nAnts){
+    int x{}, y{};
+    
+    while (nDoodleBugs > 0){
+        x = rand()%SIZE;
+        y = rand()%SIZE;
+        if (!grid[x][y]){
+            grid[x][y] = new Doodlebug(x, y);
+            nDoodleBugs--;
+        }
+    }
+    while (nAnts>0){
+        x = rand()%SIZE;
+        y = rand()%SIZE;
+        if (!grid[x][y]){
+            grid[x][y] = new Ant(x, y);
+            nAnts--;
+        }
+    }
+}
+
