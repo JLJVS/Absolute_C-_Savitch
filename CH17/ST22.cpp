52472863445a0003ea9a037ebdcda40db002e794
Jonathan <72031471+JLJVS@users.noreply.github.com>
2026-09-18 20:51:36 +0200

    Solutions for self test 21-23 Ch17

---
+++ CH17/ST22.cpp
@@ -0,0 +1,16 @@
+/*
+
+void HashTable::outputHashTable()
+{
+    Node<string> *next = hashArray[i];
+    cout << "In slot " << i << endl;
+    cout << " ";
+    while (next != NULL){
+        cout << next -> getData() << " ";
+        next = next -> getLink();
+    }
+
+    cout << endl;
+}
+
+*/
